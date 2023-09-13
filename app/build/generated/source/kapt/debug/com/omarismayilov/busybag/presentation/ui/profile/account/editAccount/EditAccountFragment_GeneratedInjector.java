9e1970ef8b5fa3ca1312db8689c6b3c20e2f0936
Omar <omarismayilov0@gmail.com>
2023-09-13 10:44:56 +0400

    Change user info in  edituserfragment

---
+++ app/build/generated/source/kapt/debug/com/omarismayilov/busybag/presentation/ui/profile/account/editAccount/EditAccountFragment_GeneratedInjector.java
@@ -0,0 +1,17 @@
+package com.omarismayilov.busybag.presentation.ui.profile.account.editAccount;
+
+import dagger.hilt.InstallIn;
+import dagger.hilt.android.components.FragmentComponent;
+import dagger.hilt.codegen.OriginatingElement;
+import dagger.hilt.internal.GeneratedEntryPoint;
+import javax.annotation.processing.Generated;
+
+@OriginatingElement(
+    topLevelClass = EditAccountFragment.class
+)
+@GeneratedEntryPoint
+@InstallIn(FragmentComponent.class)
+@Generated("dagger.hilt.android.processor.internal.androidentrypoint.InjectorEntryPointGenerator")
+public interface EditAccountFragment_GeneratedInjector {
+  void injectEditAccountFragment(EditAccountFragment editAccountFragment);
+}
